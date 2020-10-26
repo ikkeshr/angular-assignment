07d91ee1776d4a121c78c4e65d3911514f2c26a7
Ramanna, Ikkesh <ikkesh.ramanna@accenture.com>
2020-10-26 18:02:57 +0400

    form summary display form data

--- src/app/components/stepper-forms/summary/summary.component.ts
+++ src/app/components/stepper-forms/summary/summary.component.ts
@@ -20,7 +20,60 @@ export class SummaryComponent implements OnInit {
   }
 
   ngOnChanges(): void {
-    // console.log(this.generalInfoFormValues);
+    // console.log(this.pricingLocationFormValues);
+    // if (this.eventData) {
+    //   //Test Data
+    //   console.log(this.eventData);
+    //   this.generalInfoFormValues = {
+    //     category: this.eventData.category,
+    //     subcategory: this.eventData.subcategory,
+    //     type: this.eventData.type,
+    //     title: this.eventData.title,
+    //     description: this.eventData.description,
+    //     datetime: {
+    //       occurence: this.eventData.datetime.occurence,
+    //       occurence_date: this.eventData.datetime.occurence_date,
+    //       occurence_start_date: this.eventData.datetime.occurence_start_date,
+    //       occurence_end_date: this.eventData.datetime.occurence_end_date,
+    //       start_time: this.eventData.datetime.start_time,
+    //       end_time: this.eventData.datetime.end_time
+    //     },
+    //     pictures: this.eventData.pictures
+    //   }
+
+    //   this.pricingLocationFormValues = {
+    //     location: {
+    //       city: this.eventData.location.City,
+    //       venue: this.eventData.location.Venue,
+    //       address: this.eventData.location.Address
+    //     },
+    //     payment: {
+    //       type: this.eventData.payment.type,
+    //       pricings: this.eventData.payment.pricing,
+    //       methods: this.eventData.payment.methods
+    //     }
+    //   }
+
+    //   this.additionalInfoFormValues = {
+    //     restrictions: this.eventData.restrictions,
+    //     attendance_count: this.eventData.attendance_count,
+    //     special_guests: this.eventData.special_guests,
+    //     authorities_notified: this.eventData.authorities_notified
+    //   }
+    // }
+
+  }
+
+  getCurrencyCode(currency: string): string {
+    if (currency === "Indian Rupees") {
+      return "INR";
+    } else if (currency === "Mauritian Rupees") {
+      return "MUR";
+    } else if (currency === "US Dollar") {
+      return "USD";
+    } else if (currency === "Euro") {
+      return "EUR";
+    }
   }
 
 }
